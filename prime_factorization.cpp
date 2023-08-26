e36c43781844cc5341138be529a43c7d5c8d72cf
Pratik Das <76919061+pratikflies@users.noreply.github.com>
2023-08-26 17:57:05 +0530

    Update prime_factorization.cpp

--- prime_factorization.cpp
+++ prime_factorization.cpp
@@ -1,3 +1,4 @@
+Method-1:-
 void primeFactorization(int n){
   for(int i=2;i*i<=n;i++){
     if(n%i==0){
@@ -14,3 +15,35 @@ void primeFactorization(int n){
 
 TC - O(sqrt(n))
 SC - O(1)
+
+Method-2:-
+#include <bits/stdc++.h>
+using namespace std;
+
+int factor[1000001];
+void sieveFactorization(int n){
+    memset(factor,-1,sizeof(factor));
+    //have to go till n;
+    for(int i=2;i<=n;i++){
+        if(factor[i]==-1){
+            int num=i+i;
+            for(int j=num;j<=n;j+=i){
+                if(factor[j]==-1) factor[j]=i;
+            }
+        }
+    }
+}
+
+int main()
+{
+    sieveFactorization(315);
+    int n=315;
+    while(factor[n]!=-1){
+        cout<<factor[n]<<" ";
+        n/=factor[n];
+    }
+    cout<<n<<endl;
+}
+
+TC - O(nlog(longn)) for pre-processing and O(logn) per query
+SC - O(n)
